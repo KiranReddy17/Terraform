919970c021e5ec0c418a0ea00ee8c7ec1432218d
kommasaikiranreddy <saikiranreddykomma@gmail.com>
2021-12-22 04:34:48 +0000

    terraform

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_security_group_rule" "inbound" {
    cidr_blocks = var.ingress_rules.cidr_blocks
    security_group_id = "sg-0917fb4a0f76dab92"
 }
+resource "aws_security_group_rule" "other_inbound" {
+   type = "ingress"
+   from_port = var.other_ingress_rules.from_port
+   to_port = var.other_ingress_rules.to_port
+   protocol = var.other_ingress_rules.protocol
+   cidr_blocks = var.other_ingress_rules.cidr_blocks
+   security_group_id = "sg-0917fb4a0f76dab92"
+}
+
--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
 variable "ingress_rules" {
   default     = {
-    "description" = "For HTTP"
-    "from_port"   = 8080
-    "to_port"     = 8080
+    "description" = "For MySQL"
+    "from_port"   = 3306
+    "to_port"     = 3306
     "protocol"    = "tcp"
     "cidr_blocks" = ["27.59.252.52/32"]
   }
 }
-
+variable "other_ingress_rules" {
+  default     = {
+    "description" = "For RDP"
+    "from_port"   = 3389
+    "to_port"     = 3389
+    "protocol"    = "tcp"
+    "cidr_blocks" = ["0.0.0.0/0"]
+  }
+}
